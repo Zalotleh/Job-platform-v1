577ffdb4662f4da35ad48caf0ac4738c3010646e
Zalotleh <92803581+Zalotleh@users.noreply.github.com>
2023-01-20 15:37:11 +0100

    Update next.config.js
    changing the backend app url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
 //    API_URL: 'http://127.0.0.1:8000/',
-    API_URL: 'https://job-platform-django.herokuapp.com',
+    API_URL: 'sea-lion-app-c7gzh.ondigitalocean.app',
   },
 };
 
